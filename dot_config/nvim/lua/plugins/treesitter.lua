e02b88da754e44651bf266549a9d3a2bd6cb98b1
pruzicka <petr@pohybjezivot.cz>
2024-02-01 16:25:31 +0100

    changes

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,28 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
 
-    require("nvim-treesitter.configs").setup({
-      -- ensure_installed = { "javascript", "ruby" }, -- This will install the Ruby parser if it's not already installed
+    treesitter.setup({
+      ensure_installed = { "javascript", "ruby", "html", "vim", "dockerfile", "css", "markdown", "gitignore" }, -- This will install the Ruby parser if it's not already installed
+      -- auto_install = true,
       sync_install = false,
-      auto_install = true,
       ignore_install = { "all" },
       highlight = {
         enable = true, -- Enable syntax highlighting
       },
-    }),
-  },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    })
+  end,
 }
